a98f3ae5e8c62e2d58175565a14836bde8a57f33
Xiao____liu <3158299835@qq.com>
2024-08-05 13:02:44 +0800

    更新工程模板

--- STM32Project/0.江协标准库工程模板/Hardware/LED.c
+++ STM32Project/0.江协标准库工程模板/Hardware/LED.c
@@ -1,6 +1,7 @@
 #include "led.h"
-#include "stm32f10x.h"
 
+
+//初始化LED
 void LED_Init(void)
 {
     //跑马灯引脚为PB 12 13 14 15,LED为低电平有效
@@ -53,6 +54,109 @@ void LED_Init(void)
     
     GPIO_Init(GPIOB,&GPIO_InitStructure);
     GPIO_SetBits(GPIOB,GPIO_Pin_15);
-
+    
 }
 
+
+//熄灭LED
+void LED_OFF(uint8_t num)
+{
+    if(num == 0)
+    {
+        return;
+    }
+    if(num == 1)
+    {
+        GPIO_SetBits(GPIOB,GPIO_Pin_12);
+    }
+    else if(num == 2)
+    {
+        GPIO_SetBits(GPIOB,GPIO_Pin_13);
+    }
+    else if(num == 3)
+    {
+        GPIO_SetBits(GPIOB,GPIO_Pin_14);
+    }
+    else if(num == 4)
+    {
+        GPIO_SetBits(GPIOB,GPIO_Pin_15);
+    }
+}
+
+//点亮LED
+void LED_ON(uint8_t num)
+{
+    if(num == 0)
+    {
+        return;
+    }
+    if(num == 1)
+    {
+        GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    }
+    else if(num == 2)
+    {
+        GPIO_ResetBits(GPIOB,GPIO_Pin_13);
+    }
+    else if(num == 3)
+    {
+        GPIO_ResetBits(GPIOB,GPIO_Pin_14);
+    }
+    else if(num == 4)
+    {
+        GPIO_ResetBits(GPIOB,GPIO_Pin_15);
+    }
+}
+
+//反转LED
+void LED_Flip(uint8_t num)
+{
+    if(num == 0)
+    {
+        return;
+    }
+    else if(num == 1)
+    {
+        if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_12) == Bit_SET)
+        {
+            GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+        }
+        else
+        {
+            GPIO_SetBits(GPIOB,GPIO_Pin_12);
+        }
+    }
+    else if(num == 2)
+    {
+        if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_13) == Bit_SET)
+        {
+            GPIO_ResetBits(GPIOB,GPIO_Pin_13);
+        }
+        else
+        {
+            GPIO_SetBits(GPIOB,GPIO_Pin_13);
+        }
+    }
+    else if(num == 3)
+    {
+        if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_14) == Bit_SET)
+        {
+            GPIO_ResetBits(GPIOB,GPIO_Pin_14);
+        }
+        else
+        {
+            GPIO_SetBits(GPIOB,GPIO_Pin_14);
+        }
+    }
+    else if(num == 4)
+    {
+        if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_15) == Bit_SET)
+        {
+            GPIO_ResetBits(GPIOB,GPIO_Pin_15);
+        }
+        else
+        {
+            GPIO_SetBits(GPIOB,GPIO_Pin_15);
+        }
+    }
+}
--- STM32Project/0.江协标准库工程模板/User/main.c
+++ STM32Project/0.江协标准库工程模板/User/main.c
@@ -1,26 +1,17 @@
 #include "stm32f10x.h"                  // Device header
 #include "led.h"
-#include "delay.h"//直接拿的延时函数
+#include "delay.h"
+#include "Key.h"
+
 int main()
 {
 
     
     LED_Init();//初始化LED
-
+    KEY_Init();//初始化KEY
 
     while(1)
     {
-        GPIO_SetBits(GPIOB,GPIO_Pin_15);//PB15为高电平，熄灭
-        GPIO_ResetBits(GPIOB,GPIO_Pin_12);//PB12为低电平，点亮
-        Delay_ms(500);//延时500ms
-        GPIO_SetBits(GPIOB,GPIO_Pin_12);//PB12为高电平，熄灭
-        GPIO_ResetBits(GPIOB,GPIO_Pin_13);//PB13为低电平，点亮
-        Delay_ms(500);//延时500ms
-        GPIO_SetBits(GPIOB,GPIO_Pin_13);//PB13为高电平，熄灭
-        GPIO_ResetBits(GPIOB,GPIO_Pin_14);//PB14为低电平，点亮
-        Delay_ms(500);//延时500ms
-        GPIO_SetBits(GPIOB,GPIO_Pin_14);//PB14为高电平，熄灭
-        GPIO_ResetBits(GPIOB,GPIO_Pin_15);//PB15为低电平，点亮
-        Delay_ms(500);//延时500ms
+
     }
 }
