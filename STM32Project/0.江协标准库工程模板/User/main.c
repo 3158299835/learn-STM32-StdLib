a8a3c97d08485da15692df95af1a8cfaec24ba5e
Xiao____liu <3158299835@qq.com>
2024-08-05 23:54:00 +0800

    更新标准库模板--添加了一个串口

--- STM32Project/0.江协标准库工程模板/User/main.c
+++ STM32Project/0.江协标准库工程模板/User/main.c
@@ -2,7 +2,7 @@
 #include "led.h"
 #include "delay.h"
 #include "Key.h"
-
+#include "Serial.h"
 int main()
 {
 
@@ -10,13 +10,34 @@ int main()
     LED_Init();//初始化LED
     Delay_Init();//初始化延时函数
     
+    Serial_Init();//初始化串口
 
-
+    
+//    Serial_SendByte(0x41);
+//    Serial_SendByte('A');
+    
+//    uint8_t My_Array[] = {0x41,0x42,0x43,0x44};
+    
+//    char My_String[] = "Hello world";
+    
+//    Serial_SendArray(My_Array,4);
+//    Serial_SendString(My_String);
+    
+//    Serial_SendString("Hello world\n");
+//    Serial_SendNumber(12345,5);
+    
+//    Serial_Printf("num = %d",666);
+    uint8_t RxData;
     while(1)
     {
-        LED_ON(1);
-        Delay_ms(500);
-        LED_OFF(1);
-        Delay_ms(500);
+        if(Serial_GetRxfalg() == 1)
+        {
+            RxData = Serial_GetRxData();
+            Serial_SendByte(RxData);
+        }
     }
 }
+
+
+
+
