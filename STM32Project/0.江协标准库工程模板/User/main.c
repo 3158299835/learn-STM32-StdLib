faafab943b3bebf1dcf89a5bbc8f00178c623402
Xiao____liu <3158299835@qq.com>
2024-08-06 19:20:13 +0800

    更新工程文件

--- STM32Project/0.江协标准库工程模板/User/main.c
+++ STM32Project/0.江协标准库工程模板/User/main.c
@@ -1,32 +1,13 @@
 #include "stm32f10x.h"                  // Device header
-#include "led.h"
-#include "delay.h"
-#include "Key.h"
-#include "Serial.h"
 #include "oled.h"
 int main()
 {
-
-    KEY_Init();//初始化KEY
-    LED_Init();//初始化LED
-    Delay_Init();//初始化延时函数
     OLED_Init();//初始化OLED;
-    Serial_Init();//初始化串口
 
-    
-    uint8_t RxData;
+
     while(1)
     {
         
-        OLED_ShowString(1,1,"Hello World");
-        if(Serial_GetRxfalg() == 1)
-        {
-            RxData = Serial_GetRxData();
-            Serial_SendByte(RxData);
-        }
     }
 }
-
-
-
 
