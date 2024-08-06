920f46b8a923a6034b378acf8d8883e770c10088
Xiao____liu <3158299835@qq.com>
2024-08-06 12:45:16 +0800

    更新工程模板-江协的OLED

--- STM32Project/0.江协标准库工程模板/User/main.c
+++ STM32Project/0.江协标准库工程模板/User/main.c
@@ -3,33 +3,22 @@
 #include "delay.h"
 #include "Key.h"
 #include "Serial.h"
+#include "oled.h"
 int main()
 {
 
     KEY_Init();//初始化KEY
     LED_Init();//初始化LED
     Delay_Init();//初始化延时函数
-    
+    OLED_Init();//初始化OLED;
     Serial_Init();//初始化串口
 
     
-//    Serial_SendByte(0x41);
-//    Serial_SendByte('A');
-    
-//    uint8_t My_Array[] = {0x41,0x42,0x43,0x44};
-    
-//    char My_String[] = "Hello world";
-    
-//    Serial_SendArray(My_Array,4);
-//    Serial_SendString(My_String);
-    
-//    Serial_SendString("Hello world\n");
-//    Serial_SendNumber(12345,5);
-    
-//    Serial_Printf("num = %d",666);
     uint8_t RxData;
     while(1)
     {
+        
+        OLED_ShowString(1,1,"Hello World");
         if(Serial_GetRxfalg() == 1)
         {
             RxData = Serial_GetRxData();
