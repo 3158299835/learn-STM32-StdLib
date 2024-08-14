d8c4815fa65560425d76ebac08a80e8f812da583
Xiao____liu <3158299835@qq.com>
2024-08-14 21:18:19 +0800

    更新工程模板

--- STM32Project/0.江协标准库工程模板/User/main.c
+++ STM32Project/0.江协标准库工程模板/User/main.c
@@ -1,19 +1,18 @@
 #include "stm32f10x.h"                  // Device header
 #include "oled.h"
 #include "Delay.h"
-#include "key.h"
+
 
 /**
   * 函    数：
   * 参    数：
   * 返 回 值：
+  * 注意事项：
   */
-
 int main()
 {
     OLED_Init();//初始化OLED;
-
-
+    
     while(1)
     {
         
