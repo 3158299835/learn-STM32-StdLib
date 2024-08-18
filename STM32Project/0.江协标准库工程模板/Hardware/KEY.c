911b25566a2db37445f0f8aae2f721842f970d9f
Xiao____liu <3158299835@qq.com>
2024-08-18 19:57:38 +0800

    更新工程模板

--- STM32Project/0.江协标准库工程模板/Hardware/KEY.c
+++ STM32Project/0.江协标准库工程模板/Hardware/KEY.c
@@ -1,5 +1,8 @@
 #include "stm32f10x.h"                  // Device header
+#include "Delay.h"
 
+#define KEY1 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_0) == RESET
+#define KEY2 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1) == RESET
 
 void KEY_Init(void)
 {
@@ -7,13 +10,28 @@ void KEY_Init(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB,&GPIO_InitStructure);
+    Delay_Init();
     
 }
 
 uint8_t KEY_Get(void)
 {
-    return GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11);
+    if(KEY1 | KEY2)
+    {
+        Delay_ms(10);
+        if(KEY1)
+        {
+            while(KEY1);
+            return 1;
+        }
+        else if(KEY2)
+        {
+            while(KEY2);
+            return 2;
+        }
+    }
+    return 0;
 }
